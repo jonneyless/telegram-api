f5ebebc1ca78abea340b0468ed2f286b5803b573
jony <jonneyless@gmail.com>
2026-08-19 13:38:08 +0800

    增加一点便捷方法

--- models/from.go
+++ models/from.go
@@ -1,6 +1,9 @@
 package models
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // From struct
 type From struct {
@@ -13,6 +16,14 @@ type From struct {
 	LanguageCode string `json:"language_code"`
 }
 
-func (form *From) FullName() string {
-	return strings.TrimSpace(form.FirstName + " " + form.LastName)
+func (f *From) FullName() string {
+	return strings.TrimSpace(f.FirstName + " " + f.LastName)
+}
+
+func (f *From) GetMention() string {
+	if f.Username != "" {
+		return "@" + f.Username
+	}
+
+	return fmt.Sprintf(`<a href="tg://user?id=%d">%s</a>`, f.ID, f.FullName())
 }
